ff6f068e550c026578781e1abe3f071e416ef250
SAITOU Keita <keita44.f4@gmail.com>
2020-12-01 16:00:50 +0900

    Add: empty init file

---
+++ init.el
@@ -0,0 +1,20 @@
+;;; init.el --- Standard ML environment in Emacs     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  iris
+
+;; Author: iris <kei@iris>
+;; Keywords: tools, languages
+
+;; This program is distribute under MIT license.
+;; See also LICENSE file.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+
+(provide 'init)
+;;; init.el ends here
